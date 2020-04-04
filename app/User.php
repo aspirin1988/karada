8caee7329663d8dabe485d9a404830a2e7ecdcd4
sergey <aspirin_1988@mail.ru>
2020-04-04 15:41:23 +0600

    Init

--- app/User.php
+++ app/User.php
@@ -276,6 +276,7 @@ class User extends Authenticatable
                     return ($access && strtotime($access->date_end) > time() ? true : false);
                 }
             }
+            var_dump([strtotime($access->date_end), time()]);
             return ($access && strtotime($access->date_end) > time() ? true : false);
 
         }
